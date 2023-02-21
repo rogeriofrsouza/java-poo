f75704a7f9f9a1952306b1effd954b257b3b8314
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-02-21 13:39:12 -0300

    Manipulando pastas com File

---
+++ 3-java-e-programacao-orientada-a-objetos-2/01-arquivos-4/src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);  // Caminho de uma pasta ou arquivo
+		
+		File[] folders = path.listFiles(File::isDirectory);  // Reference methods: função como argumento de uma função
+		
+		System.out.println();
+		System.out.println("FOLDERS:");
+		
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		File[] files = path.listFiles(File::isFile);
+		
+		System.out.println();
+		System.out.println("FILES:");
+		
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		
+		System.out.println();
+		System.out.println("Directory created succesfully: " + success);
+		
+		sc.close();
+
+	}
+
+}
