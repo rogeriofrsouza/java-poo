3c347ad06fbd8cb45390e1725276a12cd1a148ee
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-02-21 12:59:21 -0300

    Bloco try-with-resources

--- 3-java-e-programacao-orientada-a-objetos-2/01-arquivos-2/src/application/Program.java
+++ 3-java-e-programacao-orientada-a-objetos-2/01-arquivos-2/src/application/Program.java
@@ -9,12 +9,14 @@ public class Program {
 	public static void main(String[] args) {
 		
 		String path = "D:\\Cursos\\Udemy\\3-java-poo\\repositorio\\3-java-e-programacao-orientada-a-objetos-2\\01-arquivos\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
+// 		FileReader fr = null;
+// 		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);  // Estabelece uma sequência de leitura (stream)
-			br = new BufferedReader(fr);  // Instanciado a partir do FileReader, maior abstração
+		// try-with-resources
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+//			fr = new FileReader(path);  // Estabelece uma sequência de leitura (stream)
+//			br = new BufferedReader(fr);  // Instanciado a partir do FileReader, maior abstração
 			
 			String line = br.readLine();  // Lê uma linha do arquivo, se estiver no final retorna null
 			
@@ -26,21 +28,22 @@ public class Program {
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			// Fechando as duas streams, pode lançar uma exceção
-			try {
-				if (br != null) {
-					br.close();
-				}
-				
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
+//		finally {
+//			// Fechando as duas streams, pode lançar uma exceção
+//			try {
+//				if (br != null) {
+//					br.close();
+//				}
+//				
+//				if (fr != null) {
+//					fr.close();
+//				}
+//			}
+//			catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
 		
 	}
 
