b5c7bb288e5b8882bef7b9bd1a8aedbd707b3b42
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-03-01 19:45:13 -0300

    Uma experiência com Comparator

---
+++ 3-java-e-programacao-orientada-a-objetos-2/18-funcional-comparator/src/application/MyComparator.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Comparator;
+
+import entities.Product;
+
+// Comparator objeto de classe separada
+public class MyComparator implements Comparator<Product> {
+
+	// Possui um retorno parecido com o compareTo()
+	@Override
+	public int compare(Product p1, Product p2) {
+		return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+	}
+
+}
