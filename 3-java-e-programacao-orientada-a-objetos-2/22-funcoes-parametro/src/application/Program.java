0be673756f3e33a1b172ebb64177c1baae2180df
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-03-02 16:46:51 -0300

    Criando funções que recebem funções como parâmetro

---
+++ 3-java-e-programacao-orientada-a-objetos-2/22-funcoes-parametro/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import model.services.ProductService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.0));
+		list.add(new Product("Mouse", 50.0));
+		list.add(new Product("Tablet", 350.5));
+		list.add(new Product("HD Case", 80.9));
+		
+		ProductService ps = new ProductService();
+		
+		// Função que recebe outra função lambda como argumento
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+		
+		System.out.println("Sum = $" + String.format("%.2f", sum));
+	}
+
+}
---
+++ 3-java-e-programacao-orientada-a-objetos-2/22-funcoes-parametro/src/model/services/ProductService.java
@@ -0,0 +1,27 @@
+package model.services;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import entities.Product;
+
+// Classe fechada para alteração -> mudança do filtro
+public class ProductService {
+
+	/*
+	 * Receber a condição como parâmetro da função
+	 * Condição -> Predicado retorna um boolean
+	 */
+	public double filteredSum(List<Product> list, Predicate<Product> criteria) {
+		double sum = 0.0;
+		
+		for (Product p : list) {
+			// Teste genérico de qualquer predicado recebido por parâmetro
+			if (criteria.test(p)) {
+				sum += p.getPrice();
+			}
+		}
+		
+		return sum;
+	}
+}
